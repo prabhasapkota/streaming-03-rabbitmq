c9cf7292f99abab98213899412fde9cae3e86495
Your Name <sapkotaprabha96@gmail.com>
2024-05-16 15:24:10 -0400

    Ran all python scripts

--- v1_emit_message.py
+++ v1_emit_message.py
@@ -16,10 +16,10 @@ ch = conn.channel()
 ch.queue_declare(queue="hello")
 
 # use the channel to publish a message to the queue
-ch.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+ch.basic_publish(exchange="", routing_key="hello", body="yayy, it is working")
 
 # print a message to the console for the user
-print(" [x] Sent 'Hello World!'")
+print(" [x] Sent 'yayy, it is working'")
 
 # close the connection to the server
 conn.close()
