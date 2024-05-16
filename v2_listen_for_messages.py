b88a133d9b326d13c65116455ce45d23855aa753
Your Name <sapkotaprabha96@gmail.com>
2024-05-16 15:51:44 -0400

    fixed errors for v2

--- v2_listen_for_messages.py
+++ v2_listen_for_messages.py
@@ -51,7 +51,7 @@ def process_message(ch, method, properties, body):
 # define a main function to run the program
 # pass in the hostname as a string parameter if you like
 # if no argument is provided, set a default value to localhost
-def main(hn: str = "localhosttt"):
+def main(hn: str = "localhost"):
     """Main program entry point."""
 
     # when a statement can go wrong, use a try-except block
